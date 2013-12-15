33ad9cb4dd6026f04f234af3bc3ee1c6a081577c
Ben Baker <bensemailaddress@icloud.com>
2013-12-14 20:30:45 -0600

    Create Model.js

---
+++ Model.js
@@ -0,0 +1,25 @@
+var mongoose    = require('mongoose');
+
+var modelSchema = mongoose.Schema({
+    a: String,          
+    b: Number,
+    c: { type: Array, default: [1,2,3,4] },
+});
+
+modelSchema.methods.activate = function (req, res) {
+    console.log(this.id);
+}
+
+var Model = mongoose.model('Model', modelSchema);
+
+Model.find({'id': { $ne : 1 }, function(err, results) {
+    if (err) { 
+      conaole.log('shit!'); }
+    else {
+      results.forEach( function(result) {
+        console.log(result);
+      });
+    }
+  });
+
+module.exports.Model = Model;
