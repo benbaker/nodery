f5a91f505d4f764ad7e518a4f7126bc0e6631253
Ben Baker <bensemailaddress@icloud.com>
2013-12-16 12:42:53 -0600

    Update app.js

--- app.js
+++ app.js
@@ -1,41 +1,39 @@
 // nodery 0.0.4
 
+
 // MODEL
 
-var mongoose = require('mongoose');
+var mongoose = require('mongoose')
 
 var modelSchema = mongoose.Schema({
-    a: String,          
-    b: Number,
+    a: String, b: Number,
     c: { type: Array, default: [1,2,3,4] },
-});
+})
 
 modelSchema.methods.activate = function (req, res) {
-    console.log(this.id);
+    console.log(this.id)
 }
 
-var Model = mongoose.model('Model', modelSchema);
+var Model = mongoose.model('Model', modelSchema)
 
-Model.find({'id': { $ne : 1 }, function(err, results) {
-    if (err) { 
-        console.log('could not find '); }
-    else {
+Model.find({'id': { $ne : 1 }, function( err, results ) {
+	err ? log('could not find '):
         results.forEach( function(result) {
-            console.log(result);
+            log(result);
         });
-    }
 });
 
-module.exports.Model = Model;
+module.exports.Model = Model
+
 
 // SERVER
 
-var path = require('path');
+var path = require('path')
 
 app = require('express.io')()
-app.set('port', process.env.PORT || 3000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('port', process.env.PORT || 3000)
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'jade')
 app.http().io()
 
 app.io.route('ready', function(req) {
@@ -43,9 +41,9 @@ app.io.route('ready', function(req) {
 })
 
 app.get('/', function(req, res) {
-	res.render('index');
+	res.render('index')
 })
 
 app.listen(7076)
 
-
+var log = function(thing){console.log(thing)}
