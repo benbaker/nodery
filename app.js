32af8f64caf07f651712127a405df9aca520ab04
ben <ben.navy@gmail.com>
2013-12-16 16:07:28 -0600

    simplify

--- app.js
+++ app.js
@@ -1,28 +1,16 @@
 // nodery 0.0.4
 
 var mongoose = require('mongoose')
-
-// INSTALL MONGO DB ON OSX:
-// http://docs.mongodb.org/manual/tutorial/install-mongodb-on-os-x/
-
 var modelSchema = mongoose.Schema({
-    a: String, b: Number,
-    c: { type: Array, default: [1,2,3,4] },
+    a:String,b:Number,c:{type:Array,default:[1,2]},
 })
-
 modelSchema.methods.logMe = function (req, res) {
-    log(this)
+    console.log(this)
 }
-
 var Model = mongoose.model('Model', modelSchema)
 
-module.exports.Model = Model
-
-
-// SERVER
 
 var path = require('path')
-
 app = require('express.io')()
 app.set('port', process.env.PORT || 3000)
 app.set('views', path.join(__dirname, 'views'))
@@ -30,15 +18,8 @@ app.set('view engine', 'jade')
 app.http().io()
 
 app.io.route('ready', function(req) {
-    req.io.emit('log', "sockets good");
     Model.find({}, function(err, results){
-        req.io.emit('log', "mongoose good");
         log(results);
-        if (err) {
-            req.io.emit('log', "mongoose BAD");
-        } else {
-            req.io.emit('log', "mongoose good");
-        }
     });
 })
 
@@ -48,4 +29,6 @@ app.get('/', function(req, res) {
 
 app.listen(7076);
 
-var log = function(a){console.log(a);}
+var log = function(a) { console.log(a); }
+
+// module.exports.Model = Model
